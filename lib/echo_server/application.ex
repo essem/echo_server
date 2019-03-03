84f4584417af9dc458624df31ae3f683d7e31076
essem <soomin@gmail.com>
2019-03-03 18:20:37 +0900

    Initial commit
    * mix new echo_server --sup

---
+++ lib/echo_server/application.ex
@@ -0,0 +1,20 @@
+defmodule EchoServer.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Starts a worker by calling: EchoServer.Worker.start_link(arg)
+      # {EchoServer.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: EchoServer.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
